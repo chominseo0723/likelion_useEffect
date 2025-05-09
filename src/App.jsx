502cebf2c60e0f672f19bf1443f907ddb49eb37c
chominseo <singer100@yu.ac.kr>
2025-05-09 20:00:30 +0900

    타이머 구현

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,10 @@ import React from 'react'
 import NameInput from './NameInput'
 import HookStructure from './HookStructure'
 import ItemList from './ItemList'
+import TimerSwitch from './TimerSwitch'
 
 const App = () => {
-  return <ItemList/>
+  return <TimerSwitch/>
 }
 
 export default App
